ecb3a8336fbd48f40b8e78e19bd8072d0c6548cc
newbit <newbit@gmx.de>
2021-03-17 20:34:01 +0100

    Add 4GBStick.sh

---
+++ usb-gadget/4GBStick.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+ln -sf /data/hw_init/version/region_comm/china/cdrom/4GBStick.img /data/hw_init/version/region_comm/china/cdrom/virtualstick.img
+setprop sys.usb.config mass_storage
+
