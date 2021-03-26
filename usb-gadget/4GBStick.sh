bbdefa520fa892e1df1de03bcb839510f6d8c563
newbit1 <newbit@gmx.de>
2021-03-26 20:45:33 +0100

    Added uvc-gadget from wlhe

--- usb-gadget/4GBStick.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-ln -sf /data/hw_init/version/region_comm/china/cdrom/4GBStick.img /data/hw_init/version/region_comm/china/cdrom/virtualstick.img
-setprop sys.usb.config mass_storage
-
--- usb-gadget/iso.sh
+++
@@ -1,2 +0,0 @@
-#!/system/bin/sh
-ln -sf /data/hw_init/version/region_comm/china/cdrom/linuxmint.iso /data/hw_init/version/region_comm/china/cdrom/autorun.iso
